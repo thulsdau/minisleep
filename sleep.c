2d41e2ba8c4348a556a413daa0dab6a78ff629bb
thulsdau <tohue@posteo.org>
2020-03-09 09:44:40 -0700

    Got rid of dependencies to stdlib.h and errno.h

--- sleep.c
+++ sleep.c
@@ -1,19 +1,55 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
+
+unsigned str2u(const char *str, int *error) {
+    unsigned number = 0;
+    unsigned n = 0;
+    int length;
+    int i;
+    unsigned tens = 1;
+
+    *error = 0;
+    for (length = 0; str[length] != '\0'; length++);
+    if (length == 0) {
+        *error = 1;
+        return 0;
+    }
+    for (i = 1; i <= length; i++) {
+        int digit = str[length - i] - 48;
+        if (digit < 0 || digit > 9) {
+            *error = 2;
+            return 0;
+        } 
+        n = number + (tens*digit);
+        if (n < number) {
+            *error = 3;
+            return 0;
+        } else { 
+            number = n; 
+        }
+        tens = tens * 10;
+    }
+    return number;
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 2) {
-        char *tail;
-        errno = 0;
-        unsigned long seconds = strtoul(argv[1], &tail,10);
-        if (errno == 0 && tail[0] == 0 && argv[1][0] != '-') {
-            sleep(seconds);
-            return 0;
-        } else {
-            printf("Error parsing argument, must be positive integer.\n");
+        int error = 0;
+        unsigned seconds = str2u(argv[1],&error);
+        switch (error)
+        {
+            case 0:
+                // printf("Sleeping %u seconds.\n", seconds);
+                sleep(seconds);
+                return 0;
+                break;
+            case 3:
+                printf("Argument too large, max value is %u.\n",(unsigned) -1);
+                break;
+            default:
+                printf("Error parsing argument, must be positive integer.\n");
         }
+        return 1;
     }
     printf("Usage: %s <seconds>\n",argv[0]);
     return 1;
